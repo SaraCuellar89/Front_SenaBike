82f33c5937891bee275486f70d9dea0ffc91fe9e
SaraCuellar89 <saracuellar213@gmail.com>
2025-10-15 22:14:33 -0500

    Update Carrusel_Inicio.jsx

--- src/Componentes/Carrusel_Inicio.jsx
+++ src/Componentes/Carrusel_Inicio.jsx
@@ -9,14 +9,12 @@ const Carrusel_Inicio = ({eventos}) => {
 
     //Configuracion del carrusel
     var settings = {
-        dots: true,              // Muestra los puntitos de navegación debajo del carrusel
         infinite: true,          // Permite que el carrusel vuelva al inicio al llegar al último slide (bucle infinito)
         speed: 800,            // Duración de la animación de transición entre slides (en milisegundos)
         autoplay: true,          // Activa el desplazamiento automático de los slides
         autoplaySpeed: 3000,        // Tiempo que el carrusel espera antes de pasar al siguiente slide (en milisegundos)
         pauseOnHover: true,      // Pausa el autoplay cuando el usuario pasa el mouse sobre el carrusel
         swipeToSlide: true,     // Si es true, permite arrastrar (swipe) libremente al siguiente slide con el dedo o mouse
-        arrows: true,            // Muestra las flechas izquierda/derecha para navegar manualmente
         slidesToShow: 1,         // Cantidad de slides visibles al mismo tiempo
         slidesToScroll: 1        // Cantidad de slides que avanza cada vez
     };
